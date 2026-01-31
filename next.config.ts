fac2ca1ec1a0dc994adf0da37879ff704168d509
Aditya Mahajan <adityamahajan7287@gmail.com>
2026-01-31 13:41:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
@@ -30,6 +30,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   },
 };
 
